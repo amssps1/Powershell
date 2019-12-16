894f46f0213fa1b3e6af88748d1d47b750571f6e
Pradip Shrestha <54308100+cloudstk@users.noreply.github.com>
2019-12-16 10:16:12 +0100

    Update provisioning-a-new-azure-sqlserver-db.ps1

--- provisioning-a-new-azure-sqlserver-db.ps1
+++ provisioning-a-new-azure-sqlserver-db.ps1
@@ -1,26 +1,26 @@
 # Provisioning an Azure SQL Server Database
 #region
 # 1. Authenticate your Azure subscription
-```powershell
+
 Connect-AzAccount
-```
+
 
 #endRegion
 
 #region
 # 2. Create a new resource group
- ```powershell
+
 $parameters = @{
     Name           = 'azsqldb-demo-rg'
     Location       = 'northeurope'
 }
 New-AzResourceGroup @parameters
-```
+
 #endRegion
 
 #region
 # 3. Create a Azure SQL Server
- ```powershell
+
     $Username   = "cloudstksql"
     $Password   = "pa$$w@rd1" | ConvertTo-SecureString -AsPlainText -Force
     $Cred       =  New-Object System.Management.Automation.PSCredential($Username,$Password)
@@ -34,13 +34,13 @@ $parameters = @{
 }
 
 New-AzSqlServer @parameters
-```
+
 #endRegion
 
 #region
 # 4. Create an Azure SQL Database 
 # create an Azure SQL Database, we used the following parameters:
-```powershell
+
 $parameters = @{
     ResourceGroupName               = 'azsqldb-demo-rg'
     ServerName                      = 'cloudstksqlserver'
@@ -48,14 +48,14 @@ $parameters = @{
     RequestedServiceObjectiveName   = 'S0'  
   }
 New-AzSqlDatabase @parameters
-```
+
 #endregion
 
 #region
 # 5. Setting up Firewall rules  
 # In this demo, we're going to define a specific IP address(your machine's IP) to connect SQL Azure. 
 
-```powershell
+
 $parameters = @{
     ResourceGroupName = 'azsqldb-demo-rg'
     ServerName        = 'cloudstksqlserver'
@@ -65,14 +65,14 @@ $parameters = @{
 }
 
 New-AzSqlServerFirewallRule @parameters
-```
+
 #endRegion
 
 #region
 # 6. Clean up delopment
 # To remove the resource group and all resouces associated with it.
- ```powershell
+
 Remove-AzResourceGroup -ResourceGroupName 'azsqldb-demo-rg'
-```
+
 #endRegion
 
